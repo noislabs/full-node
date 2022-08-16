d0a21d1c86c6ac633f34aa7d7c1ed2a3c7a63709
Kais <kais@aidence.com>
2022-08-16 20:40:05 +0200

    folder-structure

--- full-node/setup.sh
+++ full-node/setup.sh
@@ -1,32 +1,22 @@
 #!/bin/sh
 
-#set -ue
+set -ue
 
 BINARY_NAME=noisd
-BECH32_PREFIX=nois
-WASMD_VERSION=0.28.0
-WASMD_TAG="v$WASMD_VERSION"
-export BINARY_NAME
 
-
-if [ ! -d "wasmd" ] 
-then
-  git clone https://github.com/CosmWasm/wasmd.git
-  cd wasmd
-  git checkout "${WASMD_TAG}"
-  WASMD_COMMIT_HASH=$(git rev-parse HEAD)
-  mkdir build
-  go build \
-      -o build/$BINARY_NAME -mod=readonly -tags "netgo,ledger" \
-      -ldflags "-X github.com/cosmos/cosmos-sdk/version.Name=$BINARY_NAME \
-      -X github.com/cosmos/cosmos-sdk/version.AppName=$BINARY_NAME \
-      -X github.com/CosmWasm/wasmd/app.NodeDir=.$BINARY_NAME \
-      -X github.com/cosmos/cosmos-sdk/version.Version=${WASMD_VERSION} \
-      -X github.com/cosmos/cosmos-sdk/version.Commit=${WASMD_COMMIT_HASH} \
-      -X github.com/CosmWasm/wasmd/app.Bech32Prefix=${BECH32_PREFIX} \
-      -X 'github.com/cosmos/cosmos-sdk/version.BuildTags=netgo,ledger'" \
-      -trimpath ./cmd/wasmd
-else
-     echo "wasmd already cloned. not going to compile"
-     cd wasmd
-fi
+git clone https://github.com/CosmWasm/wasmd.git
+cd wasmd
+git checkout "${WASMD_VERSION}"
+WASMD_COMMIT_HASH=$(git rev-parse HEAD)
+mkdir build
+go build \
+    -o build/$BINARY_NAME -mod=readonly -tags "netgo,ledger" \
+    -ldflags "-X github.com/cosmos/cosmos-sdk/version.Name=$BINARY_NAME \
+    -X github.com/cosmos/cosmos-sdk/version.AppName=$BINARY_NAME \
+    -X github.com/CosmWasm/wasmd/app.NodeDir=.$BINARY_NAME \
+    -X github.com/cosmos/cosmos-sdk/version.Version=${WASMD_VERSION} \
+    -X github.com/cosmos/cosmos-sdk/version.Commit=${WASMD_COMMIT_HASH} \
+    -X github.com/CosmWasm/wasmd/app.Bech32Prefix=${BECH32_PREFIX} \
+    -X 'github.com/cosmos/cosmos-sdk/version.BuildTags=netgo,ledger'" \
+    -trimpath ./cmd/wasmd
+find .. -type f -name 'libwasm*.so' -exec cp {} build \;
